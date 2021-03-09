f8cd02192f0571a715a35aea663991b4dce93da2
Ashley Kerr <eyelovekatz@gmail.com>
2021-03-08 17:14:24 -0800

    First push

---
+++ Algo Practice - loops.js
@@ -0,0 +1,9 @@
+//Print to 1-20 only using odd numbers
+for(var num = 0; num < 20; num ++){
+    if(num % 2 == 1){
+        console.log(num);
+    }
+    }
+
+//Sum and Print 1-5 with the Num and Sum result
+
